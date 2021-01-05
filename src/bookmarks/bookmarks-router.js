b4c5c34cb5f257ae0a2de71dda1db1133a5bd69c
Rhiannon Smeby <smebyrhi@gmail.com>
2021-01-05 14:38:50 -0600

    added get/:id and delete /:id

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -56,10 +56,38 @@ bookmarksRouter
 bookmarksRouter
     .route('/bookmarks/:id')
     .get((req, res) => {
+        const {id} = req.params;
+        const bookmark = bookmarks.find(b => b.id == id);
 
+        //make sure we found a bookmark
+        if (!bookmark) {
+            logger.error(`Bookmark with id ${id} not found.`);
+            return res
+                .status(404)
+                .send('Bookmark not found')
+        }
+
+        res.json(bookmark);
     })
     .delete((req, res) => {
+        const {id} = req.params;
 
+        const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
+
+        if (bookmarkIndex === -1) {
+            logger.error(`Bookmark with id ${id} not found.`);
+            return res
+                .status(404)
+                .send('Not found');
+        }
+
+        bookmarks.splice(bookmarkIndex, 1);
+
+        logger.info(`Bookmark with id ${id} deleted`);
+
+        res
+            .status(204)
+            .end();
     })
 
 module.exports = bookmarksRouter
